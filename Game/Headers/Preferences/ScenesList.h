2f2042989177e30715b2f6610d4392a977bbf2c9
Ceffa93 <ceffa93@gmail.com>
2015-12-30 10:45:30 +0100

    The house scene now is updated to the new multishader standard

--- Game/Headers/Preferences/ScenesList.h
+++ Game/Headers/Preferences/ScenesList.h
@@ -10,7 +10,7 @@ namespace Ryno{
 	class ScenesList{
 	public:
 		static void create_scenes(){
-			//SceneManager::add_to_scenes<HouseScene>(std::string("house"));
+			SceneManager::add_to_scenes<HouseScene>(std::string("house"));
 			SceneManager::add_to_scenes<ValleyScene>(std::string("valley"));
 			SceneManager::add_to_scenes<FireworkScene>(std::string("firework"));
 		}
--- Game/Headers/Scenes/HouseScene.h
+++ Game/Headers/Scenes/HouseScene.h
@@ -1,51 +1,49 @@
-//#pragma once
-//#include "Lights/PointLight.h"
-//#include "Lights/DirectionalLight.h"
-//#include "Lights/SpotLight.h"
-//#include "DeferredRenderer.h"
-//#include <Sprite.h>
-//#include "Emitter.h"
-//#include "Scene.h"
-//#include "Game.h"
-//#include "GUIObject.h"
-//
-//namespace Ryno{
-//
-//	class HouseScene : public Scene
-//	{
-//
-//	public:
-//		HouseScene() : Scene(){}
-//	private:
-//
-//		void start() override;
-//		void input() override;
-//		void update() override;
-//
-//		Sound sound;
-//		Music music;
-//		New<Font> font;
-//
-//		New<GameObject> cones[3];
-//		New<GameObject> walls[5];
-//		New<GameObject> spot_light;
-//		New<GameObject> directional_light;
-//		New<GameObject> spheres[4];
-//		New<GameObject> emitter_obj;
-//
-//		New<GUIObject> gui;
-//
-//		GameObject* ball;
-//		std::list<GameObject*> markers;
-//
-//		I32 marker_mesh;
-//		glm::vec3 velocity = glm::vec3(0, 0, 0);
-//		glm::vec3 acceleration = glm::vec3(0, 0, 0);
-//		GameObject* marker;
-//
-//		GameObject* particle_batch;
-//
-//		GameObject* body;
-//
-//	};
-//}
+#pragma once
+#include "Lights/PointLight.h"
+#include "Lights/DirectionalLight.h"
+#include "Lights/SpotLight.h"
+#include "DeferredRenderer.h"
+#include <Sprite.h>
+#include "Emitter.h"
+#include "Scene.h"
+#include "Game.h"
+#include "Material.h"
+#include "GUIObject.h"
+
+namespace Ryno{
+
+	class HouseScene : public Scene
+	{
+
+	public:
+		HouseScene() : Scene(){}
+	private:
+
+		void start() override;
+		void input() override;
+		void update() override;
+
+		Sound sound;
+		Music music;
+		New<Font> font;
+
+		New<GameObject> cones[3];
+		New<GameObject> walls[5];
+		New<GameObject> spot_light;
+		New<GameObject> directional_light;
+		New<GameObject> spheres[4];
+		New<GameObject> emitter_obj;
+
+		New<GUIObject> gui;
+
+
+		I32 marker_mesh;
+		glm::vec3 velocity = glm::vec3(0, 0, 0);
+		glm::vec3 acceleration = glm::vec3(0, 0, 0);
+		New<Texture> bt, bn, solaire, sun, doge, white, white_normal;
+		New<I32> sphere_mesh, cone_mesh, cube_mesh, terrain_mesh;
+		New<Shader> shader;
+
+
+	};
+}
