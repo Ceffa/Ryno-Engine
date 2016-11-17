3217336fa1f52604f9220aa0944dd821709bc459
Filippo Ceffa <ceffa93@gmail.com>
2016-11-17 11:51:31 +0000

    Completed NetworkTime class

--- Ryno Engine/Headers/Network/NetworkTime.h
+++ Ryno Engine/Headers/Network/NetworkTime.h
@@ -8,14 +8,13 @@ namespace Ryno{
 	class NetworkTime {
 	public:
 		F32 network_time;
-		void new_time(F32 net_time);
+		void recv_time(F32 new_net_time);
+		void send_time(F32 new_client_time);
 
 	private:
 		F32 last_sent;					//CLIENT time of the last sent client time, used to get rtt
 
 		F32 time_samples[TIME_SAMPLES];
-		F32 sample_index;
-
-
+		U32 sample_index;
 	};
 }
--- Ryno Engine/Source/Network/NetworkTime.cpp
+++ Ryno Engine/Source/Network/NetworkTime.cpp
@@ -1,9 +1,22 @@
 #include "NetworkTime.h"
+#include "TimeManager.h"
 namespace Ryno {
 
-	void NetworkTime::new_time(F32 net_time)
+	void NetworkTime::recv_time(F32 new_net_time)
 	{
+		F32 rtt = TimeManager::time - last_sent;
+		time_samples[sample_index] = new_net_time + rtt / 2;
+		sample_index = ++sample_index %TIME_SAMPLES;
 
+		for (I32 i = 0; i < TIME_SAMPLES; i++) {
+			network_time += time_samples[i];
+		}
+		network_time /= TIME_SAMPLES;
+	}
+
+	void NetworkTime::send_time(F32 new_client_time)
+	{
+		last_sent = new_client_time;
 	}
 
 }
