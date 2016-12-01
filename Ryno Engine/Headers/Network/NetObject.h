7fb086f49344b54e2de46551027b3b6801e503b0
Filippo Ceffa <ceffa93@gmail.com>
2016-12-01 11:44:02 +0000

    Added comments to NetObject

--- Ryno Engine/Headers/Network/NetObject.h
+++ Ryno Engine/Headers/Network/NetObject.h
@@ -4,28 +4,39 @@
 #include <set>
 
 namespace Ryno{
-
+	//This struct keeps track about times
+	//and old information received through the network.
+	//Every NetworkObject holds one, and use it to predict and interpolate
 	struct TimeCache {
-		F32 lag;
-		F32 last_recv;
-		F32 times[2];
-		glm::vec3 pos[2];
-		glm::vec3 d_pos;
-		glm::quat rot[2];
-		glm::quat d_rot;
-		glm::vec3 scale[2];
-		glm::vec3 d_scale;
-		glm::vec3 last_predicted_pos;
-		glm::quat last_predicted_rot;
+		F32 lag;		//Latency of the last received packet
+		F32 last_recv;	//Local time of the last received packet
+		F32 times[2];	//Last two times received, needed to get delta_t for velocity calculation
+		//last two pos,rot and scales. Need two to calculate velocities
+		glm::vec3 pos[2];			
+		glm::quat rot[2];				
+		glm::vec3 scale[2];				
+		//velocities
+		glm::vec3 d_pos;				
+		glm::quat d_rot;				
+		glm::vec3 d_scale;				
+		//position,rotation and scale of the object when the packet is received
+		glm::vec3 last_predicted_pos;	
+		glm::quat last_predicted_rot;	
 		glm::vec3 last_predicted_scale;
+
+		//Register a new pos/rot/scale
 		void new_position(const glm::vec3& new_pos);
 		void new_rotation(const glm::quat& new_rot);
 		void new_scale(const glm::vec3& new_scale);
+		//Update times
 		void calculate_times(F32 last_net_time);
+		//Update velocities
 		void calculate_velocities();
+		//Call this function to fully update the time cache after receiving a new packet
 		void recalculate(const glm::vec3& last_pos, const glm::quat& last_rot, const glm::vec3& last_scale,F32 last_net_time);
 	};
 
+	//NetObject is the component (inherits from Script) that makes an object a network entity.
 	class NetObject : public Script {
 		friend class Client;
 		friend class NetworkScene;
@@ -35,29 +46,48 @@ namespace Ryno{
 		NetObject(const NetObject& cp) = delete;
 		NetObject() = delete;
 
+		//Holds the client id and the local object id.
+		//Together they are a univoque key
 		NetId id;
-		static std::set<NetObject*> net_objects;
-		static NetObject* find(const NetId& id);
-		NetObject* clone() override { return nullptr; }
-		void update() override;
-		void input() override {}
-		void start() override {}
-		bool is_unique() override { return true; };
-		bool is_copyable() override { return false; };
-		F32 last_received;		//needed to desync the object if inactive
 
-		//Add a new position to the time cache buffer
+		//All the network objects in the scene
+		static std::set<NetObject*> net_objects;
+
+		//Check if the network object is already instantiated
+		static NetObject* find(const NetId& id);
+
+		//Called every frame
+		void update() override;
+
+		//Keep track of the last time the object was updated 
+		//trough the network. Needed to remove object if inactive
+		F32 last_received;		
+
+		//Add a new pos/rot/scale to the time cache -> interpolation
 		void set_network_transform(const glm::vec3& new_pos, const glm::quat& new_rot, const glm::vec3& new_scale, F32 last_net_time);
-		//Hard reset of a position to avoid interpolation
+		//Hard reset of pos/rot/scale -> no interpolation
 		void reset_network_transform(const glm::vec3& new_pos, const glm::quat& new_rot, const glm::vec3& new_scale, F32 last_net_time);
 
-		U32 tag;//used to differentiate types of objects
-		F32 last_sent = 0;	//needed to send messages in regular time intervals
+		//A field used to mark different kind of objects, like a player and a ball
+		U32 tag;
+		//Keep track of the last sent packet, needed to send messages at regular intervals
+		F32 last_sent = 0;	
+		
+		//Other methods inherited from Script
+		bool is_unique() override { return true; };
+		bool is_copyable() override { return false; };
+		NetObject* clone() override { return nullptr; }
+
 	private:
+		//If true, object is destroyed at the end of frames
 		bool mark_for_destruction = false;
+		//After how long an inactive object is destroyed
 		static F32 disconnect_delay;
+		//Frequence of packet updates
 		static F32 send_delay;
+		//Holds all the information needed for interpolation and prediction
 		TimeCache time_cache;
+		//If true, this objects sends update, if false it receives them
 		bool owned;
 	};
 
--- Ryno Engine/Source/Network/NetObject.cpp
+++ Ryno Engine/Source/Network/NetObject.cpp
@@ -11,6 +11,7 @@ namespace Ryno {
 	NetObject::NetObject(const NetId& addr) : id(addr), last_sent(0) {
 		net_objects.insert(this);
 		Client* c = Network::get_instance()->client;
+		//If the address of the object is the same as the client, object is owned
 		owned = id.client_id == c->client_id;
 	}
 
@@ -23,20 +24,28 @@ namespace Ryno {
 	}
 
 	void NetObject::set_network_transform(const glm::vec3& new_pos,const glm::quat& new_rot, const glm::vec3& new_scale, F32 last_net_time) {
-		//Avoid duplicates and out of order
+		//Solves duplicate and out of order problem
+		//by ignoring packets with a timestamp older
+		//then the last packet received
 		if (time_cache.times[0] - last_net_time > -.00001f)
 			return;
-		time_cache.new_position(new_pos);//probably add lerp here between estimated and actual pos
-		time_cache.new_rotation(new_rot);//probably add lerp here between estimated and actual pos
-		time_cache.new_scale(new_scale);//probably add lerp here between estimated and actual pos
 
+		//Add to the time cache the data from the last packet
+		//and recalculate the time cache based on them
+		time_cache.new_position(new_pos);
+		time_cache.new_rotation(new_rot);
+		time_cache.new_scale(new_scale);
 		time_cache.recalculate(game_object->transform.get_position(),game_object->transform.get_rotation(),game_object->transform.get_scale(),last_net_time);
 	}
 
 	void NetObject::reset_network_transform(const glm::vec3& new_pos, const glm::quat& new_rot, const glm::vec3& new_scale, F32 last_net_time) {
-		//Avoid duplicates and out of order
+		//Solves duplicate and out of order problem
+		//by ignoring packets with a timestamp older
+		//then the last packet received
 		if (time_cache.times[0] - last_net_time > -.00001f)
 			return;
+
+		//Hard reset pos/rot/scale, times and many other members
 		time_cache.pos[0] = new_pos;
 		time_cache.pos[1] = new_pos;
 		time_cache.rot[0] = new_rot;
@@ -58,9 +67,11 @@ namespace Ryno {
 
 
 	void TimeCache::recalculate(const glm::vec3& last_pos, const glm::quat& last_rot, const glm::vec3& last_scale, F32 last_net_time) {
+		//Set current state as last predicted states
 		last_predicted_pos = last_pos;
 		last_predicted_rot = last_rot;
 		last_predicted_scale = last_scale;
+		//Update times and velocities
 		calculate_times(last_net_time);
 		calculate_velocities();
 	}
@@ -68,16 +79,17 @@ namespace Ryno {
 	void NetObject::update()
 	{
 		if (!owned) {
-			F32 delta_t_loc = TimeManager::time - time_cache.last_recv;
-			F32 lerp_net = glm::clamp(delta_t_loc / send_delay, 0.0f, 1.0f);
-			
 			//Calculate the predicted transform when a packet is received.
-			//This is different from the previously predicted transform, this is based on the last packet, 
-			//and the prediction is what could happen during the latency
+			//That is: the last received transform plus a prediction based on
+			//the amount of latency of the packet
 			glm::vec3 start_pos = time_cache.pos[0] + time_cache.lag * time_cache.d_pos;
 			glm::quat start_rot = glm::slerp(time_cache.rot[0], time_cache.rot[0] * time_cache.d_rot, time_cache.lag);
 			glm::vec3 start_scale = time_cache.scale[0] + time_cache.lag * time_cache.d_scale;
 
+			//Find the lerp factor based on the time elapsed since last packet received
+			F32 delta_t_loc = TimeManager::time - time_cache.last_recv;
+			F32 lerp_net = glm::clamp(delta_t_loc / send_delay, 0.0f, 1.0f);
+			
 			//Interpolate between the last predicted transform and the current predicted transform
 			start_pos = ryno_math::lerp(time_cache.last_predicted_pos, start_pos, lerp_net);
 			start_rot = glm::lerp(time_cache.last_predicted_rot, start_rot, lerp_net);
@@ -88,39 +100,47 @@ namespace Ryno {
 			start_rot = glm::slerp(start_rot, start_rot * time_cache.d_rot, delta_t_loc);
 			start_scale += delta_t_loc * time_cache.d_scale;
 			
-			//Set the new transform
+			//Set the new transform to the game object
 			game_object->transform.set_position(start_pos);
 			game_object->transform.set_rotation(start_rot);
 			game_object->transform.set_scale(start_scale);
 		}
 	}
 	void TimeCache::calculate_times(F32 last_net_time) {
+		//Shift the times buffer
 		times[1] = times[0];
 		times[0] = last_net_time;
+		//Calculate the latency
 		lag = Network::client->net_time.get_time() - last_net_time;
+		//Record last packet received
 		last_recv = TimeManager::time;
 	}
 	void TimeCache::new_position(const glm::vec3& newPos) {
+		//Shift pos buffer
 		pos[1] = pos[0];
 		pos[0] = newPos;
 	}
 	void TimeCache::new_rotation(const glm::quat& new_rot) {
+		//Shift rot buffer
 		rot[1] = rot[0];
 		rot[0] = new_rot;
 	}
 	void TimeCache::new_scale(const glm::vec3& new_scale) {
+		//Shift scale buffer
 		scale[1] = scale[0];
 		scale[0] = new_scale;
 	}
 	void TimeCache::calculate_velocities() {
-		//Get delta time and error check for nan
+		//Get velocities as derivatives of the last two received transforms
+		//over the interval of time between receiving them.
+		//Check for zero division errors
 		F32 time_delta = times[0] - times[1];
 		if (time_delta < .00001f)
 			time_delta = .00001f;
+
 		d_pos = (pos[0] - pos[1]) / time_delta;
 		d_rot = glm::slerp(glm::quat(glm::vec3(0,0,0)),(glm::inverse(rot[0]) * rot[1]),1.0f/time_delta);
 		d_scale = (scale[0] - scale[1])/time_delta;
-	
 	}
 	
 }
